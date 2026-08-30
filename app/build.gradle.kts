b953edf47282cfb1876bb5487939052d9c5db650
therealmangoosey <126335809+therealmangoosey@users.noreply.github.com>
2026-08-30 01:57:49 +0100

    Fix AGP 9 built-in Kotlin configuration

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     id("com.android.application")
-    id("org.jetbrains.kotlin.android")
 }
 
 android {
@@ -28,9 +27,5 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
-    }
-
-    kotlinOptions {
-        jvmTarget = "17"
     }
 }
