cb6e0cb99c1b1d323c2acc0b66b5f99853310854
therealmangoosey <126335809+therealmangoosey@users.noreply.github.com>
2026-08-30 02:37:56 +0100

    Bump App Monitor to 1.1.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.therealmangoosey.appmonitor"
         minSdk = 26
         targetSdk = 36
-        versionCode = 4
-        versionName = "1.1.3"
+        versionCode = 5
+        versionName = "1.1.4"
     }
 
     buildFeatures {
